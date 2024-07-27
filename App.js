74fc0b4a7695553e51dd6469caca93e00cdc824e
jackhdewey <jackhdewey@gmail.com>
2024-07-27 09:11:17 -0400

    Added Mongo interface

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import "dotenv/config";
 import express from 'express';
 import cors from "cors";
+import mongoose from 'mongoose';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import UserRoutes from "./User/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
@@ -15,5 +21,6 @@ Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
