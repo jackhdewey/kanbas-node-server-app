058d5b66e8b66c9cd23e44f3e4f2e47e44c86647
jackhdewey <jackhdewey@gmail.com>
2024-08-04 16:29:13 -0400

    A6 updates

--- User/routes.js
+++ User/routes.js
@@ -1,18 +1,29 @@
 import * as dao from "./dao.js";
+
 let currentUser = null;
 export default function UserRoutes(app) {
   const createUser = async (req, res) => { };
   const deleteUser = async (req, res) => { };
+
   const findAllUsers = async (req, res) => { 
+    const { role } = req.query;
+    if (role) {
+      const users = await dao.findUserByRole(role);
+      res.json(users);
+      return;
+    }
     const users = await dao.findAllUsers(); 
     res.json(users) 
-};
+    return;
+  };
+  
   const findUserById = async (req, res) => { };
   const updateUser = async (req, res) => { };
   const signup = async (req, res) => { };
   const signin = async (req, res) => { };
   const signout = (req, res) => { };
   const profile = async (req, res) => { };
+
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
--- User/schema.js
+++ User/schema.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     firstName: String,
-    email: String,
     lastName: String,
+    email: String,
     dob: Date,
     role: {
       type: String,
