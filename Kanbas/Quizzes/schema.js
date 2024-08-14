8a37848173586a7a782f06a15109939ffad55f6d
jackhdewey <jackhdewey@gmail.com>
2024-08-14 17:18:42 -0400

    Project updates

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -2,23 +2,55 @@ import mongoose from "mongoose";
 
 const quizSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    qType: {
+    quiz_type: {
       type: String,
       enum: ["GRADED", "PRACTICE", "GRADED SURVEY", "UNGRADED SURVEY"],
-      default: "GRADED",
+      default: "GRADED"
     },
     points: Number,
     group: {
       type: String,
       enum: ["QUIZZES", "EXAMS", "ASSIGNMENTS", "PROJECT"],
-      default: "QUIZZES",
+      default: "QUIZZES"
     },
-    dueDate: Date,
-    availableData: Date,
-    untilDate: Date,
-    timeLimit: Number,
-    multipleAttempts: { type: Boolean, default: true},
-    course: { type: String, required: true },
+    questions: [],
+    shuffle_answers: { 
+      type: String, 
+      enum: ["YES", "NO"],
+      default: "NO"
+    },
+    time_limit: {type: Number, default: 1},
+    multiple_attempts: { 
+      type: String, 
+      enum: ["YES", "NO"],
+      default: "NO"
+    },
+    num_attempts: {type: Number, default: 1},
+    show_answers: { 
+      type: String, 
+      enum: ["YES", "NO"],
+      default: "NO"
+    },
+    access_code: { type: String, default: ""},
+    one_question_at_a_time: { 
+      type: String, 
+      enum: ["YES", "NO"],
+      default: "YES"
+    },
+    webcam: { 
+      type: String, 
+      enum: ["YES", "NO"],
+      default: "NO"
+    },
+    lock_after_answering: { 
+      type: String, 
+      enum: ["YES", "NO"],
+      default: "NO"
+    },
+    due_date: String,
+    date_available: String,
+    available_until: String,
+    course: { type: String, required: true }
   },
   { collection: "quizzes" }
 );
