7f5cd4f8252269e8ad6a16ec4d6518cb6e9f3573
jackhdewey <jackhdewey@gmail.com>
2024-08-16 16:10:27 -0400

    Project updates

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -47,7 +47,7 @@ export default function UserRoutes(app) {
       res.status(400).json({message: "Username already taken"});
       return;
     }
-    currentUser = await dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
     req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
